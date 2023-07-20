da38a48c15488a991237b4a09495aa21a68170bb
jacksonmonteiro <jackson.monteiro@gfxconsultoria.com>
2023-07-20 13:58:54 -0300

    Chess Position class created

---
+++ Chess/ChessPosition.cs
@@ -0,0 +1,21 @@
+﻿using Chess.Board;
+
+namespace Chess.Chess {
+    internal class ChessPosition {
+        public char Column { get; set; }
+        public int Line { get; set; }
+
+        public ChessPosition(char column, int line) {
+            Column = column;
+            Line = line;
+        }
+
+        public Position toPosition() {
+            return new Position(8 - Line, Column - 'a');
+        }
+
+        public override string ToString() {
+            return $"{Column}{Line}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,20 +4,9 @@ using Chess.Chess;
 namespace Chess {
     internal class Program {
         static void Main(string[] args) {
-            ChessBoard board = new ChessBoard(8, 8);
-
-            try {
-                board.insertPiece(new Tower(board, Color.Black), new Position(0, 9));
-
-                // board.insertPiece(new Tower(board, Color.Black), new Position(1, 3));
-                // board.insertPiece(new King(board, Color.Black), new Position(2, 4));
-                Screen.printBoard(board);
-            }
-            catch (BoardException e) {
-                Console.WriteLine(e.Message);
-            }
-
-
+            ChessPosition pos = new ChessPosition('a', 1);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosition());
         }
     }
 } 
